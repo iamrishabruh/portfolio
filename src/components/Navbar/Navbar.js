7d1ac13edd751f6571165ba8638cc6dadedc10e7
Rishabh Chouhan <rishabruh@Rishabhs-MacBook-Pro-2.local>
2024-09-30 11:10:19 -0400

    Fixed more imports

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import Brightness2Icon from '@material-ui/icons/Brightness2';
 import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
-import { ThemeContext } from '../../contexts/theme';
+import { ThemeContext } from '../../theme';
 import { projects, skills, contact, leadership } from '../../portfolio';
 import './Navbar.css';
 
