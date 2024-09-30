cc1bce36dc1c33652bb785051bafc9c0d17b6192
Rishabh Chouhan <rishabruh@Rishabhs-MacBook-Pro-2.local>
2024-09-30 13:38:35 -0400

    Finalized

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/iamrishabruh/my-personal-portfolio/'
+      href='https://github.com/iamrishabruh/portfolio/'
       className='link footer__link'
     >
       @ Rishabh Chouhan 2024
