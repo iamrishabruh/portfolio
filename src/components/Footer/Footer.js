5559f5fbafbb8f102e37b3681c42995cd389f38a
Rishabh Chouhan <rishabruh@Rishabhs-MacBook-Pro-2.local>
2024-10-16 14:44:09 -0400

    Testing changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
       href='https://github.com/iamrishabruh/portfolio/'
       className='link footer__link'
     >
-      @ Rishabh Chouhan 2024
+      @ Rishabruh Chouhan 2024
     </a>
   </footer>
 )
